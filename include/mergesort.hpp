82447576febccddc2ca728deea88a1b01a922c96
dagt07 <davidgarciat07@gmail.com>
2025-05-01 18:41:09 -0400

    Refac: Project structure initial commit

---
+++ include/mergesort.hpp
@@ -0,0 +1,6 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+//#include <vector>
+
+//acá dejar CONST que se vayan a usar para mergesort, ejemplo: M_SIZE, B_SIZE, etc.
---
+++ include/quicksort.hpp
@@ -0,0 +1,6 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+//#include <vector>
+
+//acá dejar CONST que se vayan a usar para quicksort, ejemplo: M_SIZE, B_SIZE, etc.
