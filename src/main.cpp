f66a6057dc71fa0b4487d7dca8e2dd3ab8f34910
dagt07 <davidgarciat07@gmail.com>
2025-05-04 23:16:32 -0400

    Docs: pesudo code ideas updated

--- src/main.cpp
+++ src/main.cpp
@@ -23,17 +23,19 @@
 //main:
     
     //for(int i = 0; i < 15; i++) {
-        //1)llamar a la función que genera los 5 archivos para un único tamaño N <- haciendo todo para no ocupar tanto espacio en disco
+        //1) llamar a la función que genera los 5 archivos para un único tamaño N <- haciendo todo para no ocupar tanto espacio en disco
 
-        //2)llamado a mergesort sobre cada uno de los 5 archivos de tamaño N. 
+        //2) determinar a
 
-        //3)llamado a quicksort sobre cada uno de los 5 archivos de tamaño N. 
+        //3) llamado a mergesort sobre cada uno de los 5 archivos de tamaño N. 
 
-        //4)promediar los resultados para ese N
+        //4) llamado a quicksort sobre cada uno de los 5 archivos de tamaño N. 
 
-        //5)guardar los resultados en un archivo de salida (csv o txt)
+        //5) promediar los resultados para ese N
 
-        //6) liberar memoria
+        //6) guardar los resultados en un archivo de salida (csv o txt)
+
+        //7) liberar memoria
 
     //y más cosas a implementar...
 
